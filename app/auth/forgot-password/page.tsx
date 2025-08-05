5d7ca1ec23561f072b3e4dc80cd21901fcc4e04b
serkanasli <asliserkan@yandex.com>
2025-08-05 11:31:42 +0300

    refactor: updated auth layout structure

--- app/auth/forgot-password/page.tsx
+++ app/auth/forgot-password/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import AppLogo from "@/components/app-logo";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -33,52 +32,40 @@ function Page() {
   };
 
   return (
-    <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
-        <div className="flex items-center justify-center">
-          <AppLogo
-            image={{
-              width: 200,
-              height: 200,
-            }}
-          />
-        </div>
-        <Card>
-          <CardHeader>
-            <CardTitle className="text-xl">Reset your password</CardTitle>
-            <CardDescription>
-              Enter your email address and we’ll send you a link to reset your password.
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <form onSubmit={handleSubmit} id="login-form">
-              <div className="flex flex-col gap-6">
-                <div className="grid gap-2">
-                  <Label htmlFor="email">Email</Label>
-                  <Input id="email" type="email" placeholder="email@example.com" />
-                </div>
-              </div>
-            </form>
-          </CardContent>
-          <CardFooter className="flex-col gap-2">
-            <Button
-              form="login-form"
-              disabled={isLoading}
-              type="submit"
-              className="w-full"
-              variant="blue"
-              size="lg"
-            >
-              {isLoading && <Loader2Icon className="animate-spin" />}
-              Send reset password email
-            </Button>
-            <Button disabled={isLoading} className="ml-auto" asChild variant="link">
-              <Link href="/auth/login">Log in</Link>
-            </Button>
-          </CardFooter>
-        </Card>
-      </div>
-    </div>
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-xl">Reset your password</CardTitle>
+        <CardDescription>
+          Enter your email address and we’ll send you a link to reset your password.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form onSubmit={handleSubmit} id="login-form">
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" type="email" placeholder="email@example.com" />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex-col gap-2">
+        <Button
+          form="login-form"
+          disabled={isLoading}
+          type="submit"
+          className="w-full"
+          variant="blue"
+          size="lg"
+        >
+          {isLoading && <Loader2Icon className="animate-spin" />}
+          Send reset password email
+        </Button>
+        <Button disabled={isLoading} className="ml-auto" asChild variant="link">
+          <Link href="/auth/login">Log in</Link>
+        </Button>
+      </CardFooter>
+    </Card>
   );
 }
 
--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,22 +1,7 @@
-import AppLogo from "@/components/app-logo";
 import { LoginForm } from "@/components/auth/login-form";
 
 function Page() {
-  return (
-    <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
-        <div className="flex items-center justify-center">
-          <AppLogo
-            image={{
-              width: 200,
-              height: 200,
-            }}
-          />
-        </div>
-        <LoginForm mode="signup" />
-      </div>
-    </div>
-  );
+  return <LoginForm mode="signup" />;
 }
 
 export default Page;
